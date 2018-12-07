e5be4b03d0b4bee620f4d6ce9bcbca7a91eb33a2
Souyahibou <souaybou.k@live.fr>
2018-12-07 19:52:25 +0100

    93th solve db:seeds problems for reservation_vol

--- engines/thp/week/6/day/1/reservation_vol/db/migrate/20171106190101_create_reservation_vol_flights.rb
+++ engines/thp/week/6/day/1/reservation_vol/db/migrate/20171106190101_create_reservation_vol_flights.rb
@@ -3,10 +3,10 @@ class CreateReservationVolFlights < ActiveRecord::Migration[5.1]
     create_table :reservation_vol_flights do |t|
       t.integer :duration
       t.datetime :departure_date
-      # t.references :departure_airport, foreign_key: true
-      # t.references :arrival_airport, foreign_key: true
-      t.references :reservation_vol_departure_airport, foreign_key: false, index: { :name => "index_flights_on_departure_airport_id"}
-      t.references :reservation_vol_arrival_airport, foreign_key: false, index: { :name => "index_flights_on_arrival_airport_id"}
+      t.references :departure_airport, foreign_key: false #true to false
+      t.references :arrival_airport, foreign_key: false   #true => false
+      # t.references :reservation_vol_departure_airport, foreign_key: false, index: { :name => "index_flights_on_departure_airport_id"}
+      # t.references :reservation_vol_arrival_airport, foreign_key: false, index: { :name => "index_flights_on_arrival_airport_id"}
 
       t.timestamps
     end
